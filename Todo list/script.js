9bfbd40de1ded5f6da9e842fafb99df32a553bae
kalyanadapa <kalyankumaradapa821@gmail.com>
2023-08-22 11:21:37 +0530

    Made changes for delete functionality

--- Todo list/script.js
+++ Todo list/script.js
@@ -23,7 +23,7 @@ listContainer.addEventListener("click",function(e){
     if(e.target.tagName==="LI"){
         e.target.classList.toggle("checked");
         saveData()
-    }else if(e.target.tagName==='span'){
+    }else if(e.target.tagName==="SPAN"){
         e.target.parentElement.remove();
         saveData()
     }
